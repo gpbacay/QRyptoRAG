ac98e7f6007e1a4c468f7e47f9e6a4a1438a73eb
Gianne BACAY <gpbacay00118@usep.edu.ph>
2025-11-13 15:27:18 +0000

    add more left and right paddings in this page

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -16,7 +16,7 @@ export default function DocsLayout({
     <SidebarProvider>
       <div className="flex min-h-screen flex-col">
         <Header />
-        <div className="flex-1 items-start md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
+        <div className="flex-1 items-start md:grid md:grid-cols-[220px_1fr] md:gap-12 lg:grid-cols-[240px_1fr] lg:gap-16">
           <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
             <ScrollArea className="h-full py-6 pr-6 lg:py-8">
               <SidebarNav />
