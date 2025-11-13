d56455a472b57676a7937c4ede204bde76f5e1ec
Gianne BACAY <gpbacay00118@usep.edu.ph>
2025-11-13 15:28:33 +0000

    add a left padding in this left nav bar

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -18,7 +18,7 @@ export default function DocsLayout({
         <Header />
         <div className="flex-1 items-start md:grid md:grid-cols-[220px_1fr] md:gap-12 lg:grid-cols-[240px_1fr] lg:gap-16">
           <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-            <ScrollArea className="h-full py-6 pr-6 lg:py-8">
+            <ScrollArea className="h-full py-6 px-6 lg:py-8">
               <SidebarNav />
             </ScrollArea>
           </aside>
