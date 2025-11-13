1a310993d384e27e7697a26b0667e739c9f19417
Gianne BACAY <gpbacay00118@usep.edu.ph>
2025-11-13 15:30:23 +0000

    in the searchbar, remove the icon that is only for mac because this webs

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -59,7 +59,7 @@ export function Search() {
         <span className="hidden lg:inline-flex">Search documentation...</span>
         <span className="inline-flex lg:hidden">Search...</span>
         <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.4rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
+          <span className="text-xs">Ctrl</span>K
         </kbd>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
